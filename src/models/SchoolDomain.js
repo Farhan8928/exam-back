8f455577751e310d4127ee31da3649f1e9dd172c
Farhan Sayyed <sayyedfarhan8286@gmail.com>
2026-03-02 15:40:10 +0530

    remove js

--- src/models/SchoolDomain.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const schoolDomainSchema = new mongoose.Schema({
-  domain: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("SchoolDomain", schoolDomainSchema);
